565af2cf03ac2b8c8f1f65c862d50ed189f89d29
Virat Dagar <dagarvirat8@gmail.com>
2026-08-27 10:24:45 +0530

    Assignment updated

--- javascript/callback.js
+++ javascript/callback.js
@@ -4,8 +4,22 @@ function sum(a,b){
 
 function sumwithmsg(clbck,msg){
     const result=clbck(20,30);
-    const fresult=msg+"your score is "+result;
+    const fresult=msg +"your score is "+result;
     console.log(fresult);
 
 }
-sumwithmsg(sum,"hey Deepak");
+sumwithmsg(sum,"hey Deepak ");
+
+//function-2
+
+function sub(x,y){
+    return x-y;
+}
+
+function Subwithmsg(callbck,msg){
+    const result=callbck(50,30);
+    const Sresult=msg + "your score is " + result;
+    console.log(Sresult);
+
+}
+Subwithmsg(sub,"hey Deepak ");
