997c68c5c9ffb3873b2169ad8bb5687aa5914620
Virat Dagar <dagarvirat8@gmail.com>
2026-09-24 09:11:10 +0530

    react folder added

---
+++ React/virat-project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <Card/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ React/virat-project/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+
+        <div style={{border:'2px solid red', height:'300px',width:'200px',margin:'20px',padding:'10px'}}>
+        
+        <h5 style={{color:'red'}} >  STUDENT ID CARD</h5>
+         <div> <img src="https://www.animaker.com/hub/wp-content/uploads/2023/03/Mickey_Mouse_Disney_1.webp" alt="profile" style={{width:'100px',height:'100px',border:'2px solid'}} /></div>
+         <h5>Name: MICKY</h5>
+         <h5>Class: CSE DS</h5>
+        <h5>YEAR:3rd year</h5>
+        </div>
+      
+
+    </div>
+  )
+}
+
+export default Card
